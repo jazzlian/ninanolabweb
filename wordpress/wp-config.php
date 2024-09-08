a05cbd881b57e2bd33d3d6534e9c1d84cbeaecfb
MyoungSigYoun <jazzlian@gmail.com>
2024-09-08 22:14:12 +0900

    2024.09.08 add file

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mysql-server' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
